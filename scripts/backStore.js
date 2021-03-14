3c33fcde9d1d7d779b5692692b2b77a024ec2324
BigSaladMan <jonathanpauze25@gmail.com>
2021-03-14 16:04:46 -0400

    cursed javascript code for new button

---
+++ scripts/backStore.js
@@ -0,0 +1,3 @@
+function createNewOrder() {
+  document.getElementById("orderSection").innerHTML += '<ul class="list-group list-group-horizontal"><li class="list-group-item User">New user</li><li class="list-group-item ProductList">No current Order</li><li class="list-group-item Edit"><button type="button" class="btn btn-primary">Edit</button></li><li class="list-group-item Delete"><button type="button" class="btn btn-danger">Delete</button></li></ul>';
+}
