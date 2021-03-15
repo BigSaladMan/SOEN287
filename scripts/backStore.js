3eec369c19ac6f19d81dad9b37bbdf9c58f1e689
BigSaladMan <jonathanpauze25@gmail.com>
2021-03-15 18:08:23 -0400

    Very ghetto javascript for delete added  and some edit QOL

--- scripts/backStore.js
+++ scripts/backStore.js
@@ -1,22 +1,49 @@
 let counter = 0;
 let target = 0;
+
 function createNewOrder() {
     counter++;
-   document.getElementById("orderSection").innerHTML += `<ul class="list-group list-group-horizontal" id = "${counter}"><li class="list-group-item User">New user</li><li class="list-group-item ProductList">No current products</li><li class="list-group-item Edit"><button type="button" class="btn btn-primary"  onclick="findTarget(${counter})" data-toggle="modal"data-target="#exampleModal" >Edit</button></li><li class="list-group-item Delete"><button type="button" class="btn btn-danger">Delete</button></li></ul>`;
-
+    document.getElementById("orderSection").innerHTML += 
+   `<ul class="list-group list-group-horizontal" id = "${counter}">
+        <li class="list-group-item User" id= "${"u"+counter}">New user</li>
+        <li class="list-group-item ProductList" id="${"p"+counter}">No current products</li>
+        <li class="list-group-item Edit">
+            <button type="button" class="btn btn-primary" onclick="findTarget(${counter})" data-toggle="modal"data-target="#exampleModal" >Edit</button>
+        </li>
+        <li class="list-group-item Delete">
+            <button type="button" onclick="deleteCurrentOrder(${counter})" class="btn btn-danger">Delete</button>
+        </li>
+    </ul>`;
 }
+
 function findTarget(vari){
- target = vari;
- console.log(vari);
+    target = vari;
+    setDefaultFields();
 }
 
 function editCurrentOrder() {
     let x = document.getElementById("editUsersName").value;
     let y = document.getElementById("editUsersOrder").value;
-   document.getElementById(target).innerHTML = `<li class="list-group-item User">${x}</li><li class="list-group-item ProductList">${y}</li><li class="list-group-item Edit"><button type="button" class="btn btn-primary"  onclick="findTarget(${counter})" data-toggle="modal"data-target="#exampleModal" >Edit</button></li><li class="list-group-item Delete"><button type="button" class="btn btn-danger">Delete</button></li>`;
+    document.getElementById(target).innerHTML = 
+   `<li class="list-group-item User" id = "${"u"+ target}">${x}</li>
+    <li class="list-group-item ProductList" id= ${"p"+target}>${y}</li>
+    <li class="list-group-item Edit">
+        <button type="button" class="btn btn-primary"  onclick="findTarget(${target})" data-toggle="modal"data-target="#exampleModal" >Edit</button>
+    </li>
+    <li class="list-group-item Delete">
+        <button type="button" onclick="deleteCurrentOrder(${target})" class="btn btn-danger">Delete</button>
+    </li>`;
 }
 
-function deleteCurrentOrder(counter) {
-    let x = document.getElementById(counter).innerHTML;
-    let y = document.getElementById(counter).innerHTML;
-  }
+function deleteCurrentOrder(deletion) {
+    document.getElementById(deletion).remove();
+}
+
+function setDefaultFields() {
+    let a = "u" + target;
+    let b = "p" + target; 
+    let c = document.getElementById(a).innerHTML;
+    let d = document.getElementById(b).innerHTML;
+    document.getElementById("editUsersName").value = c;
+    document.getElementById("editUsersOrder").value = d;
+}
